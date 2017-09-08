ba3b3dfadb4e2ddfadc319aa34e1addbc2921a76
Kevin Colyer <kevin@ywambrussels.be>
2017-09-08 19:22:49 +0100

    updated

--- test.go
+++ test.go
@@ -7,6 +7,25 @@ import (
 	"gitlab.com/wmlph/poloniex-api"
 )
 
+type price float64
+type amount float64
+type Order poloniex.WSOrder
+
+func (p price) String() (s string) {
+    s=fmt.Sprintf("%0.8d",p)
+    return 
+}
+
+func (a amount) String() (s string) {
+    s=fmt.Sprintf("%0.8d",a)
+    return
+}
+
+func (o Order) String() (s string) {
+    s=fmt.Sprintf("Type: %v Rate: %v Amount: %v",o.Type,o.Rate,o.Amount)
+    return
+}
+
 // func (t PrivateTradeHistoryEntry) String() string {
 //     fmt.Sprintf("(%s) Trade: ",t.Date,t.Type, )
 // }
@@ -31,21 +50,42 @@ func main() {
 // 		log.Fatalln(err)
 // 	}
 // 	fmt.Printf("%+v\n", balances)
-mytrades, err := p.PrivateTradeHistoryAllWeek()
+        // my Trades
+        fmt.Println("My Trades")
+        mytrades, err := p.PrivateTradeHistoryAllWeek()
 // 	mytrades, err := p.PrivateTradeHistory("BTC_ETH")
 // 	mytrades, err := p.OpenOrders("BTC_ETH")
-// mytrades, err := p.OpenOrdersAll()
+//      mytrades, err := p.OpenOrdersAll()
         if err != nil {
 		log.Fatalln(err)
 	}
 	fmt.Printf("%+v\n", mytrades)
+        
+	// Prices
         ticker,err:=p.Ticker()
         if err != nil {
 		log.Fatalln(err)
 	}
+        fmt.Println("\nPrices")
 	fmt.Printf("%+v\n", ticker["USDT_BTC"])
         USDT_BTC:=ticker["USDT_BTC"].Last
         fmt.Println("Last price of Bitcoin was: ", USDT_BTC)
+        
+        // Balances
+        fmt.Println("\nBalances")
+       	balances, err := p.Balances()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%+v\n", balances)
+
+        // my open OpenOrdersAll
+        fmt.Println("\nMy Open Orders")
+        openorders, err := p.OpenOrdersAll()
+         if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%+v\n", openorders)
 }
 
 /* intialise
